f10114ef54c3c46b8894688fbc40c26a707a0b29
Yuki Kikuchi <kikku650@gmail.com>
2022-08-08 18:04:20 +0900

    タップしたマスの色を替えるようにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 //import 'dart:html';
+import 'package:sprintf/sprintf.dart';
+
 import './config/size_config.dart';
 import 'package:flutter/material.dart';
 import './gamearea.dart';
@@ -20,6 +22,27 @@ class MyApp extends StatefulWidget {
 class _State extends State<MyApp> {
   var _scaffoldKey = new GlobalKey<ScaffoldState>();
 
+  List<List<Color>> currentcolors =
+      List.generate(9, (_) => List.generate(9, (_) => Colors.white));
+  List<List<String>> currentnumbers =
+      List.generate(9, (_) => List.generate(9, (_) => ""));
+
+  _State() {
+    initializecolors();
+  }
+
+  void initializecolors() {
+    currentcolors =
+        List.generate(9, (_) => List.generate(9, (_) => Colors.white));
+  }
+
+  void updatearea(int row, int column) {
+    setState(() {
+      initializecolors();
+      currentcolors[row][column] = Colors.blue;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -29,37 +52,76 @@ class _State extends State<MyApp> {
         title: Text('GridView demo'),
       ),
       body: Container(
-        child: gamearea(),
-      ),
+          child: Scaffold(
+              body: Center(
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "sudoku insanity",
+              style: TextStyle(fontSize: 40),
+            ),
+          ),
+          sudokucontainer(),
+        ]),
+      ))),
     );
   }
 
-  Widget _generator(int index) {
-    return GestureDetector(
-      child: GridTile(
-        child: Container(
-          color: Colors.grey,
-          child: Center(
-            child: Text(
-              '${index + 1}',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-        ),
-      ),
-      onDoubleTap: () {
-        final snackBar = SnackBar(
-          content: const Text('Yay! A SnackBar!'),
-          action: SnackBarAction(
-            label: 'Undo',
-            onPressed: () {
-              // Some code to undo the change.
-            },
-          ),
-        );
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      },
+  Widget sudokucontainer() {
+    double side = SizeConfig.blockSizeHorizontal! * 90;
+    return Container(
+      color: Colors.blue.shade100,
+      width: side,
+      height: side,
+      child: Stack(children: [
+        for (int row = 0; row < 3; row++) ...{
+          for (int column = 0; column < 3; column++) ...{
+            Align(
+              alignment: Alignment(row - 1, column - 1),
+              child: Container(
+                width: side / 3,
+                height: side / 3,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.green),
+                  //borderRadius: BorderRadius.circular(10),
+                ),
+                child: Table(
+                    border: TableBorder.all(color: Colors.white),
+                    columnWidths: const <int, TableColumnWidth>{
+                      0: FlexColumnWidth(1.0),
+                      1: FlexColumnWidth(1.0),
+                      2: FlexColumnWidth(1.0),
+                    },
+                    defaultVerticalAlignment: TableCellVerticalAlignment.top,
+                    children: [
+                      for (int i = 0; i < 3; i++) ...{
+                        TableRow(children: [
+                          for (int j = 0; j < 3; j++) ...{
+                            Align(
+                              alignment: Alignment(i - 1, j - 1),
+                              child: Container(
+                                width: side / 9,
+                                height: side / 9,
+                                color: currentcolors[3 * row + j]
+                                    [3 * column + i],
+                                child: TextButton(
+                                  child: Text(sprintf(
+                                      "%i,%i", [3 * row + j, 3 * column + i])),
+                                  onPressed: () =>
+                                      updatearea(3 * row + j, 3 * column + i),
+                                ),
+                              ),
+                            )
+                          }
+                        ])
+                      }
+                    ]),
+              ),
+            )
+          }
+        }
+      ]),
     );
   }
 }
